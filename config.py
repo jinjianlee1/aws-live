32ccc11a94220acbbfeeda3ee47bfc48cd8b6169
jinjianlee1 <112604974+jinjianlee1@users.noreply.github.com>
2022-09-19 20:15:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee1.cqiljpncokas.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leejinjian-employee"
+customregion = "us-east-1"
